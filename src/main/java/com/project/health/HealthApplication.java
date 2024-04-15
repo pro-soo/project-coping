16203b95bfe5c66dbeba30e0b2c854cabcbb3fe2
zdemit <zdemit1@gmail.com>
2024-04-15 11:01:23 +0900

    build.gradle war 추가

--- src/main/java/com/project/health/HealthApplication.java
+++ src/main/java/com/project/health/HealthApplication.java
@@ -2,10 +2,16 @@ package com.project.health;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HealthApplication {
+public class HealthApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HealthApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(HealthApplication.class, args);
 	}
